77c1f4443bb1719288090382b260004851555bd1
BUGINGO Janvier <66186428+BugingoJanvier@users.noreply.github.com>
2025-04-03 18:10:28 +0200

    first commit connection added to the DB

---
+++ models/tasks.js
@@ -0,0 +1,25 @@
+import { Model } from 'sequelize';
+
+'use strict';
+
+export default (sequelize, DataTypes) => {
+  class Tasks extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Tasks.init({
+    title: DataTypes.STRING,
+    description: DataTypes.STRING,
+    completed: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'Tasks',
+  });
+  return Tasks;
+};
